417c9c17e7af8f307a562915e25c19b0bbd14107
Shariar13 <88611911+Shariar13@users.noreply.github.com>
2021-10-05 22:43:37 +0600

    Update settings.py

--- helpme/settings.py
+++ helpme/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS =[
 ]
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
-STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles') 
 
 
 
